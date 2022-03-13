f11a3941f8f48818495e4b431df42cd775513fa4
Jason <jasonpaff@gmail.com>
2022-03-13 19:10:45 -0400

    deployment changes

--- server.js
+++ server.js
@@ -27,11 +27,11 @@ server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
+app.use(express.static(path.join(__dirname,'build')));
 app.use(logger('dev'));
 app.use(cors({origin: process.env.APP_ORIGIN}));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
-app.use(express.static(path.join(__dirname,'build')));
 app.use(checkJwt);
 
 app.get('/', (req, res) => {
