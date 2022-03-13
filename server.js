d79bc2afd3f00298b2d7e14d42c9939f16e7a281
Jason <jasonpaff@gmail.com>
2022-03-13 18:11:10 -0400

    login page cleanup

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const {auth} = require('express-oauth2-jwt-bearer');
 const cors = require("cors");
 const {graphTicketSchema} = require("./graphQLSchemas");
 const {rootResolver} = require("./graphQLResolvers");
+const path = require("path");
 require('dotenv').config();
 
 const checkJwt = auth({
@@ -30,6 +31,7 @@ app.use(logger('dev'));
 app.use(cors({origin: process.env.APP_ORIGIN}));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.static(path.join(__dirname, 'build')));
 //app.use(checkJwt);
 
 // graphQL query/mutation endpoint
