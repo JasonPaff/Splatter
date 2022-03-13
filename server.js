4066e7333d484f00f731ad084d9e98c18aedbc79
Jason <jasonpaff@gmail.com>
2022-03-13 18:57:48 -0400

    deployment changes

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(express.static(path.join(__dirname,'build')));
 app.use(checkJwt);
-
+client
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'));
 });
