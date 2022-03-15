d71ee40b2ec2fb26939d8a8196bd3a75dd3a2c08
Jason <jasonpaff@gmail.com>
2022-03-15 16:20:54 -0400

    switch to apollo server

--- src/utils/routeUtility.js
+++ src/utils/routeUtility.js
@@ -1,4 +1,4 @@
 ﻿//export const apiRoute = 'http://localhost:4000';
 //export const apiRouteWs = 'ws://localhost:4000';
 export const apiRoute = 'https://splatter-app.herokuapp.com';
-export const apiRouteWs = 'ws://splatter-app.herokuapp.com';
+export const apiRouteWs = 'wss://splatter-app.herokuapp.com';
